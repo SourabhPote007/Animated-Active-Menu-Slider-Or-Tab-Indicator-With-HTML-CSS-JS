a78f251b1b279b25736b1ef850af2dd257acf385
Sourabh Anil Pote <110386969+SourabhPote007@users.noreply.github.com>
2023-10-26 17:27:40 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+let slider = document.querySelector(".slider");
+let li = document.querySelectorAll(".navigation ul li");
+
+let index_value = 0;
+let left_position = 0;
+
+li.forEach((item,index) => {
+    slider.style.width = li[0].clientWidth + "px";
+    slider.style.left = left_position + "px";
+
+    item.onclick = function(){
+        slider.style.width = item.clientWidth + "px";
+        // console.log(index);
+        index_value = index;
+        get_left_position();
+        slider.style.left = left_position + "px";
+        left_position = 0;
+    }
+});
+
+//Getting Left Position For Slider To Slide.
+function get_left_position(){
+    for (let i = 0; i < index_value; i++) {
+        const element = li[i];
+        left_position += element.clientWidth;
+        // console.log(left_position);
+    }
+}
